005744a26cefcb25354e14e6f0899443c1e8faf7
Ted Lawless <lawlesst@gmail.com>
2016-11-04 15:18:27 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,70 @@
+## InCites Retrieve
+
+This code supports batch retrieval of metrics from the [InCites Benchmarking and Analytics](http://ipscience.thomsonreuters.com/product/incites/) [API](http://ipscience-help.thomsonreuters.com/inCites2Live/10053-TRS.html).
+
+For more information about web services for the Web of Science, please review this [data integration](http://ip-science.interest.thomsonreuters.com/data-integration) website.
+
+# Getting started
+
+### Requirements
+* Python 2.7 or 3.5
+* Access to the [InCites API](http://ipscience-help.thomsonreuters.com/inCites2Live/10053-TRS.html)
+
+### Install
+
+* Download or clone this repository by clicking on the "Clone or download" button in the top right corner
+* Set an environment variable with your API Key.
+ * INCITES_KEY
+
+#### Setting environment variables
+
+On a Mac or Linux system:
+
+~~~
+$ export INCITES_KEY="mykey"
+~~~
+
+On Windows, open a command window:
+
+~~~
+set INCITES_KEY="mykey"
+~~~
+
+#### Running a script
+
+Run the script with the incoming csv data as the first parameter. The output will be printed as CSV to the console. It can be redirected to a file for saving. For example:
+
+~~~
+$ python batch_lookup.py myfile.csv > output.csv
+~~~
+
+#### Disclaimer
+
+These scripts are provided to allow InCites users to perform common operations with the AMR web service. The scripts and uses cases may change over time.
+
+## Use Cases
+
+### Retrieve InCites metrics for a set of documents
+
+With a CSV file containing a `ut` column, lookup each item via InCites and save retrieved metrics to a file.
+
+#### incoming data
+|ut|
+|----|
+|WOS:000180378500003
+|WOS:000259986300005
+|WOS:000235983900007
+|WOS:000253436400011
+
+#### matched data
+
+ISI_LOC|NCI|IMPACT_FACTOR|AVG_EXPECTED_RATE|...
+---|---|---|---|---|
+000180378500003|4.38|-1|32.4385|...
+000259986300005|6.59|8.166|21.2582|...
+000235983900007|.87|4.155|34.3393|...
+000253436400011|2.42|5.153|24.3601|...
+
+Partial output. See full output in the [documentation](http://about.incites.thomsonreuters.com/api/).
+ 
+
---
+++ batch_lookup.py
@@ -0,0 +1,90 @@
+"""
+Look up batches of UTs in InCites.
+
+Run as:
+
+$ python batch_lookup.py sample_file.csv outputfile.csv 
+
+"""
+from __future__ import print_function
+
+import csv
+import json
+import os
+import sys
+import time
+
+# Python 2 or 3
+PYV = sys.version_info
+if PYV > (3, 0):
+    from urllib.request import urlopen
+    from urllib.parse import urlencode
+    from itertools import zip_longest as zipl
+else:
+    from urllib2 import urlopen
+    from urllib import urlencode
+    from itertools import izip_longest as zipl
+
+
+URL = "https://api.thomsonreuters.com/incites_ps/v1/DocumentLevelMetricsByUT/json"
+INCITES_KEY = os.environ['INCITES_KEY']
+# Number of UTs to send to InCites at once - 100 is limit set by API.
+BATCH_SIZE = 100
+
+
+def grouper(iterable, n, fillvalue=None):
+    """
+    Group iterable into n sized chunks.
+    See: http://stackoverflow.com/a/312644/758157
+    """
+    args = [iter(iterable)] * n
+    return zipl(*args, fillvalue=fillvalue)
+
+
+def eprint(*args, **kwargs):
+    """
+    Utility for printing to stderr.
+    http://stackoverflow.com/a/14981125/758157
+    """
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def get(batch):
+    data = []
+    params = urlencode({'X-TR-API-APP-ID': INCITES_KEY, 'UT': ",".join([b for b in batch if b is not None])})
+    url = "{}?{}".format(URL, params)
+    rsp = urlopen(url)
+    raw = json.loads(rsp.read().decode('utf-8'))
+    data = [item for item in raw['api'][0]['rval']]
+    return data
+
+
+def main():
+    
+    found = []
+    to_check = []
+    with open(sys.argv[1]) as infile:
+        for row in csv.DictReader(infile):
+            d = {}
+            for k, v in row.items():
+                if k.lower().strip() == "ut":
+                    to_check.append(v.strip().replace("WOS:", ""))
+
+    found = []
+    
+    writer = csv.writer(sys.stdout)
+    first = True
+    for idx, batch in enumerate(grouper(to_check, BATCH_SIZE)):
+        eprint("Processing batch", idx)
+        found = get(batch)
+        for grp in found:
+            if first is True:
+                # write header
+                writer.writerow(grp.keys())
+                first = False
+            writer.writerow(grp.values())
+        time.sleep(.5)
+
+
+if __name__ == "__main__":
+    main()
